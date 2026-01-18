4f1987260177dde7eeafbc38d3fc15665c16acd4
Josh Stein <josh.e.stein@gmail.com>
2026-01-18 14:29:09 +0200

    Don't put dr address on newlines

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def generate_invoice(data, template_path: Path, output_path: Path):
     data["doctor_name"] = os.getenv("DOCTOR_NAME")
     data["practice_phone"] = os.getenv("PRACTICE_PHONE")
     data["practice_email"] = os.getenv("PRACTICE_EMAIL")
-    data["practice_address"] = os.getenv("PRACTICE_ADDRESS").replace("\\n", "\n")
+    data["practice_address"] = os.getenv("PRACTICE_ADDRESS")
     data["practice_number"] = os.getenv("PRACTICE_NUMBER")
     data["mp_number"] = os.getenv("MP_NUMBER")
 

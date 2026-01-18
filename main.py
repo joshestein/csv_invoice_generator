0062a37bfcc3c4ccc1f67b667b87ee3c5cf811d1
Josh Stein <josh.e.stein@gmail.com>
2026-01-18 14:10:12 +0200

    Start generating pdf

--- main.py
+++ main.py
@@ -1,15 +1,42 @@
-from pathlib import Path
 import os
-import pandas as pd
+from pathlib import Path
 
+import pandas as pd
+from dotenv import load_dotenv
+from jinja2 import Template
+from weasyprint import HTML
+
+load_dotenv()
 
 def read_invoice(path: Path):
     df = pd.read_csv(path)
     print(df)
+    return df
+
+
+def generate_invoice(data, template_path: Path, output_path: Path):
+    with open(template_path, 'r') as f:
+        template = Template(f.read())
+
+    data["doctor_name"] = os.getenv("DOCTOR_NAME")
+    data["doctor_phone"] = os.getenv("DOCTOR_PHONE")
+    data["doctor_email"] = os.getenv("DOCTOR_EMAIL")
+    data["practice_address"] = os.getenv("PRACTICE_ADDRESS").replace("\\n", "\n")
+    data["practice_number"] = os.getenv("PRACTICE_NUMBER")
+    data["mp_number"] = os.getenv("MP_NUMBER")
+
+    data["bank_name"] = os.getenv("BANK_NAME")
+    data["bank_account"] = os.getenv("BANK_ACCOUNT")
+    data["bank_code"] = os.getenv("BANK_CODE")
+
+    html_out = template.render(**data)
+    HTML(string=html_out).write_pdf(output_path)
+    print(f"Invoice generated at {output_path}")
 
 
 def main():
-    read_invoice(Path(os.getcwd()) / "invoices" / "test.csv")
+    data = read_invoice(Path(os.getcwd()) / "invoices" / "test.csv")
+    generate_invoice(data, Path('invoice_template.html'), Path('output/invoice.pdf'))
 
 
 if __name__ == "__main__":
